ec1f494f1c0bcf1a949f0fded2fde90b8fa380ff
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-10-08 04:25:40 -0400

    Add list of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import todoLogo from './assets/todo.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import todoLogo from "./assets/todo.svg";
+import "./App.css";
+
+const todoList = [
+  {
+    id: 1,
+    title: "Item 1",
+  },
+  {
+    id: 2,
+    title: "Item 2",
+  },
+  {
+    id: 3,
+    title: "Item 3",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="" target="_blank" rel="noreferrer">
-          <img src={todoLogo} className="logo" alt="Todo logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a href="" target="_blank" rel="noreferrer">
+        <img src={todoLogo} className="logo" alt="Todo logo" />
+      </a>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((todoItem) => (
+          <li key={todoItem.id}>{todoItem.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
